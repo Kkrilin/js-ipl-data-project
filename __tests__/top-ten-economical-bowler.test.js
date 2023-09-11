3c04fb1cc8fdb17802f935ad149a35b97a361825
kundan singh patel <kundansingh066@gmail.com>
2023-09-11 10:31:55 +0530

    change in the logic of topteneconomicbowler

--- __tests__/top-ten-economical-bowler.test.js
+++ __tests__/top-ten-economical-bowler.test.js
@@ -39,13 +39,9 @@ test('top ten economical bowler in 2015', () => {
   expect(
     topTenEconomicalBowler(sampleMatches, sampleDeliveries, 2),
   ).toMatchObject([
-    { bowler: 'thakur', economy: 6 },
-    { bowler: 'ishan', economy: 12 },
-    { bowler: 'suresh', economy: 18 },
+    { bowler: 'ishant', economy: 10 },
+    { bowler: 'umesh', economy: 12 },
+    { bowler: 'kumar', economy: 16 },
     { bowler: 'shami', economy: 18 },
-    { bowler: 'nehra', economy: 24 },
-    { bowler: 'starc', economy: 24 },
-    { bowler: 'ashwin', economy: 24 },
-    { bowler: 'jadeja', economy: 24 },
   ]);
 });
--- src/server/top-ten-economical-bowler-logic.js
+++ src/server/top-ten-economical-bowler-logic.js
@@ -15,7 +15,7 @@ const topTenEconomicalBowler = function (matches, deliveries, minBowled) {
     }
   }
   for (let bowler in runConced) {
-    if (ballBowled[bowler] > minBowled) {
+    if (ballBowled[bowler] < minBowled) {
       delete ballBowled[bowler];
       delete runConced[bowler];
     } else {
@@ -36,3 +36,40 @@ const topTenEconomicalBowler = function (matches, deliveries, minBowled) {
 };
 
 module.exports = topTenEconomicalBowler;
+
+console.log(
+  topTenEconomicalBowler(
+    [
+      { id: 1, season: '2015' },
+      { id: 2, season: '2015' },
+      { id: 3, season: '2015' },
+      { id: 4, season: '2015' },
+      { id: 5, season: '2015' },
+      { id: 6, season: '2015' },
+      { id: 7, season: '2015' },
+      { id: 8, season: '2015' },
+    ],
+    [
+      { match_id: 1, bowler: 'kumar', total_runs: '1', wide_runs: '0' },
+      { match_id: 1, bowler: 'umesh', total_runs: '2', wide_runs: '0' },
+      { match_id: 2, bowler: 'ishant', total_runs: '3', wide_runs: '0' },
+      { match_id: 2, bowler: 'nehra', total_runs: '4', wide_runs: '0' },
+      { match_id: 3, bowler: 'starc', total_runs: '4', wide_runs: '0' },
+      { match_id: 3, bowler: 'suresh', total_runs: '3', wide_runs: '0' },
+      { match_id: 4, bowler: 'ashwin', total_runs: '4', wide_runs: '0' },
+      { match_id: 4, bowler: 'jadeja', total_runs: '4', wide_runs: '0' },
+      { match_id: 5, bowler: 'thakur', total_runs: '1', wide_runs: '0' },
+      { match_id: 5, bowler: 'ishan', total_runs: '2', wide_runs: '0' },
+      { match_id: 6, bowler: 'kumar', total_runs: '4', wide_runs: '0' },
+      { match_id: 6, bowler: 'shami', total_runs: '4', wide_runs: '0' },
+      { match_id: 7, bowler: 'umesh', total_runs: '3', wide_runs: '0' },
+      { match_id: 7, bowler: 'ishant', total_runs: '1', wide_runs: '0' },
+      { match_id: 7, bowler: 'umesh', total_runs: '2', wide_runs: '0' },
+      { match_id: 8, bowler: 'kumar', total_runs: '3', wide_runs: '0' },
+      { match_id: 8, bowler: 'shami', total_runs: '2', wide_runs: '0' },
+      { match_id: 8, bowler: 'umesh', total_runs: '1', wide_runs: '0' },
+      { match_id: 8, bowler: 'ishant', total_runs: '1', wide_runs: '0' },
+    ],
+    2,
+  ),
+);
